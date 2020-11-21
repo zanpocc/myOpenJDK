17aedde7032f46e96ddf80c369784cf16af823b1
zanpocc <zanpocc@gmail.com>
2020-11-21 20:25:13 +0800

    deleted .DS_Store

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 source ./env.bash
 ./configure clean
-./configure
-make all
+./configure OBJCOPY=gobjcopy --with-debug-level=slowdebug --enable-debug-symbols ZIP_DEBUGINFO_FILES=0 
+make CONF=macosx-x86_64-normal-server-slowdebug
--- env.bash
+++ env.bash
@@ -1,3 +1,4 @@
+# 语言选项
 export LANG=C
 export CC=clang
 # 警告看作错误
@@ -7,25 +8,35 @@ export USE_CLANG=true
 export LP64=1
 # 64位
 export ARCH_DATA_MODEL=64
+# 运行自动下载依赖
 export ALLOW_DOWNLOADS=true
-# 四个线程并发
-export HOTSPOT_BUILD_JOBS=4
-export ALT_PARALLEL_COMPILE_JOBS=4
+# 四个线程并发编译
+export HOTSPOT_BUILD_JOBS=8
+# export ALT_PARALLEL_COMPILE_JOBS=4
+# 不比较镜像差异
 export SKIP_COMPARE_IMAGES=true
+# 使用预编译头
 export USE_PRECOMPILED_HEADER=true
+
 export INCREMENTAL_BUILD=true
+# 需要编译的内容
 export BUILD_LANGTOOLS=true
 export BUILD_JAXP=true
 export BUILD_JAXWS=true
 export BUILD_CORBA=true
 export BUILD_HOTSPOT=true
 export BUILD_JDK=true
-export SKIP_DEBUG_BUILD=true
-# 跳过快速调试版本的构建
-export SKIP_FASTDEBUG_BUILD=false
+# 要编译的版本
+export SKIP_DEBUG_BUILD=false
+export SKIP_FASTDEBUG_BUILD=true
 export DEBUG_NAME=debug
 export BUILD_DEPLOY=false
+# 不生成安装文件
 export BUILD_INSTALL=false
+
+# 调试用
+# export OBJCOPY=gobjcopy
+
 # 你的bootstrap jdk的地址，得比当前版本低
 export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.7.0_80.jdk/Contents/Home
 
